5f7e4cb8af9792cae16265afedced3926bdcf421
Leonardo Chiarelli <devleochiarelli@gmail.com>
2024-07-07 00:36:24 -0300

    Create Primeiro-Comando.php

---
+++ PHP/PHP Básico/Primeiro-Comando.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Primeiro exemplo de PHP</title>
+    <style>
+        .php{
+            color: red;
+            font-size: 2em;
+            text-align: center;
+            text-shadow: 3px 3px 3px rgba(0, 0, 0, 0.342);
+        }
+    </style>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php 
+        echo "<p class='php'>Olá, Mundo!</p>"; // Para escrever na tela, podemos colocar tag's html e estilizar com css
+    ?>
+</body>
+</html>
