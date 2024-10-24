1209ce63fed6a25dedcfba7012202477ab0621e1
Leonardo Chiarelli <devleochiarelli@gmail.com>
2024-10-24 02:24:05 -0300

    update

--- Java/IdiomaDoSistema/src/idiomadosistema.java
+++
@@ -1,8 +0,0 @@
-import java.util.Locale;
-
-public class idiomadosistema{
-    public static void main(String[] args){
-        Locale loc = Locale.getDefault();
-        System.out.print("O idioma do sistema operacional é o " + loc.getDisplayLanguage());
-    }
-}
