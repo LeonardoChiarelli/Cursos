55a840d0b645ee5ada650e87e1b89cb88eee676c
Leonardo Chiarelli <devleochiarelli@gmail.com>
2024-11-13 00:41:11 -0300

    update

--- Java/Spring/src/main/java/br/com/LeoChiarelli/main/Main.java
+++ Java/Spring/src/main/java/br/com/LeoChiarelli/main/Main.java
@@ -44,7 +44,7 @@ public class Main {
                 .collect(Collectors.toList());
         // .toList() gera uma lista imutável
 
-        System.out.println("\nTop 10 episódios: ");
+        System.out.println("\nTop 5 episódios: ");
         dadosEpisodios.stream()
                 .filter(e -> !e.rating().equalsIgnoreCase("N/A"))
                 .sorted(Comparator.comparing(EpisodesData::rating).reversed())
@@ -58,6 +58,19 @@ public class Main {
 
         episodios.forEach(System.out::println);
 
+
+        System.out.println("Informe o trecho do título que deseja buscar: ");
+        var trechoTitulo = leitor.nextLine();
+        Optional<Episode> episodioBuscado = episodios.stream() // 'Optional<>' - espécie de container // atribuindo o trecho à uma variável
+                .filter(e -> e.getTitle().toUpperCase().contains(trechoTitulo.toUpperCase())) // se o título do episódio contiver (contains()) o que foi passado pelo usuário
+                .findFirst(); // 'findAny()' para um resultado mais rápido, porém menos preciso ; 'findFirst()' - para pegar sempre o mesmo resultado
+        if(episodioBuscado.isEmpty()){ // para verificar se a variável está vazia
+            System.out.println("Episódio não encotrado! Verifique a digitação");
+        }else{
+            System.out.println("Temporada: " + episodioBuscado.get().getTemporada() + "\nEpisódio número " + episodioBuscado.get().getEpisode());
+        }
+
+
         System.out.println("À partir de que ano você deseja ver os episódios? ");
         var ano = leitor.nextInt();
         leitor.nextLine();
@@ -69,18 +82,33 @@ public class Main {
                 .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca)) // Queremos uma data que seja posterior à data informada pelo usuário 'isAfter()'
                 .forEach(e -> System.out.println(
                         "Temporada " + e.getTemporada() + "\n" +
-                        "Episódio " + e.getEpisode() + "\n" +
-                        "Título: '" +e.getTitle() + "'\n" +
-                        "Data de Lançamento: " + e.getDataLancamento().format(formatador) + "\n"
+                                "Episódio " + e.getEpisode() + "\n" +
+                                "Título: '" +e.getTitle() + "'\n" +
+                                "Data de Lançamento: " + e.getDataLancamento().format(formatador) + "\n"
                 ));
+
+        Map<Integer, Double> avaliacoesPorTem = episodios.stream()
+                .filter(e -> e.getRating() > 0.0)
+                .collect(Collectors.groupingBy(Episode::getTemporada, Collectors.averagingDouble(Episode::getRating))); // Coletamos os episódios e agrupamos por temporada e mostrar a média de avalições de cada temporada
+        // System.out.println(avaliacoesPorTem);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getRating() > 0.0)
+                .collect(Collectors.summarizingDouble(Episode::getRating));
+        System.out.printf("""
+                Média de avaliações: %.2f/10
+                Menor avaliação: %.2f/10
+                Maior avaliação: %.2f/10
+                Foram avaliados %d episódios para a montagem das estatísticas
+                """, est.getAverage(), est.getMin(), est.getMax(), est.getCount());
     }
 }
 
 // '->' ou '::' - são lambdas, chamadas de funções anônimas - são uma maneira de definir funções que são frequentemente usadas uma única vez, direto no local onde elas serão usadas.
-    // (argumentos) -> { corpo-da-função }
-        // (a, b) -> { return a + b; }
-            // List<Integer> lista = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-            // lista.stream().filter(i -> i % 2 == 0).forEach(System.out::println);
+// (argumentos) -> { corpo-da-função }
+// (a, b) -> { return a + b; }
+// List<Integer> lista = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+// lista.stream().filter(i -> i % 2 == 0).forEach(System.out::println);
 
 // 'stream' - fluxo de dados
 // 'peek' - verifcar cada etapa do código
