0a7ac1fe2f66dbfb346c936178856d710af196bb
Leonardo Chiarelli <devleochiarelli@gmail.com>
2025-05-01 04:09:57 -0300

    up

---
+++ Python/Python crie a sua primeira aplicação/app.py
@@ -0,0 +1,8 @@
+print("Sabor Express \n"
+"1. Cadastrar Restaurante \n"
+"2. Listar Restaurantes \n"
+"3. Ativar Restaurante \n"
+"4. Sair \n")
+
+respotaUsuario = input("Qual ação você deseja?: ")
+print("Você escolheu a opção: ", respotaUsuario)
